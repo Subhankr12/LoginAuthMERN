4c4bfd0eda23e633436c942e332c81bf113839db
Subhankr12 <subhankr12@gmail.com>
2019-12-23 11:22:27 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://subhankr12:hogrider123@loginauth-shard-00-00-gfnuh.mongodb.net:27017,loginauth-shard-00-01-gfnuh.mongodb.net:27017,loginauth-shard-00-02-gfnuh.mongodb.net:27017/test?ssl=true&replicaSet=LoginAuth-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "jhfkjhkafhkjha"
+}
